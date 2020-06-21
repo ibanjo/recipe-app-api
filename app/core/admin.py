011cb2b7024e24795fd9f282005dbf23d3da15d4
Giuseppe Maria D'Aucelli <giuseppe.daucelli@gmail.com>
2020-06-21 20:34:58 +0200

    Added create tags featurs

--- app/core/admin.py
+++ app/core/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.utils.translation import gettext as _
 
-from . import models
+from app.core import models
 
 
 class UserAdmin(BaseUserAdmin):
@@ -28,3 +28,4 @@ class UserAdmin(BaseUserAdmin):
 
 
 admin.site.register(models.User, UserAdmin)
+admin.site.register(models.Tag)
